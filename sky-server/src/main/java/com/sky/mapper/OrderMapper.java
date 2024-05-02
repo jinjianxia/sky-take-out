e064e2f4c7a571b0aa1d0774c93a86d39370162c
jinjianxia <jianxia1999@gmail.com>
2024-05-02 14:35:01 +0800

    订单支付功能代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,7 +2,11 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.core.annotation.Order;
+
+import java.time.LocalDateTime;
 
 /**
  * ClassName: OrderMapper
@@ -14,5 +18,20 @@ import org.springframework.core.annotation.Order;
  */
 @Mapper
 public interface OrderMapper {
-    void insert(Orders order);
+    void insert(Orders order);	/**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{check_out_time} where id = #{id}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, Long id);
+
 }
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -18,5 +18,6 @@ public interface UserMapper {
     User getByOpenid(String openid);
 
     void insert(User user);
-
+@Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
