e917536a81f98ae8d722aa4b96948fd42e6da28a
jinjianxia <jianxia1999@gmail.com>
2024-05-02 20:29:01 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -33,4 +33,6 @@ public interface ShoppingCartMapper {
 
     @Delete("delete from shopping_cart where id = #{id}")
     void deleteById(Long id);
+
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
